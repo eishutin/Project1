d5663e21f34ae87ef67c44f724268f161c255502
Евгений <evg.ishutin@gmail.com>
2019-06-19 00:29:54 +0300

    add database configurations add versions to upload add git ignore file bugs fixed

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const uuid = require('uuid/v4');
 
 
 router.post("/", async function (req, res) {
@@ -8,37 +9,50 @@ router.post("/", async function (req, res) {
 
     const name = req.query.name;
     const password = req.query.password;
+
     if (!name) {
         return res.status(400).send('Name is invalid.');
     }
-    if (Object.keys(req.files).length === 0) {
+    if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files were uploaded.');
     }
 
     if (!password) {
         return res.status(400).send('Password is invalid.');
     }
+    const filever = await File.count({name: name});
+    const filedb = await File.findOne({name: name, ver: filever});
 
-    const filedb = await File.findOne({name: name});
     if (!filedb) {
         const sampleFile = req.files[Object.keys(req.files)[0]];
         const filepath = path.normalize(__dirname + '/../files/' + name);
         sampleFile.mv(filepath, async function (err) {
             if (err)
                 return res.status(500).send(err);
-            await File.create({name: name, password: password, path: filepath});
-            res.send('File uploaded!');
+            try {
+                await File.create({name: name, password: password, path: filepath, ver: 1});
+                return res.send('File uploaded!');
+            } catch (e) {
+                return res.status(500).send(err);
+            }
         });
     } else {
         if (password === filedb.password) {
             const sampleFile = req.files[Object.keys(req.files)[0]];
-            sampleFile.mv(path.normalize(__dirname + '/../files/' + name), function (err) {
+            const filepath = path.normalize(__dirname + '/../files/' + uuid());
+            sampleFile.mv(filepath, async function (err) {
                 if (err)
                     return res.status(500).send(err);
+                try {
+                    await File.create({name: name, password: password, path: filepath, ver: filever + 1});
+
+                } catch (e) {
+                    return res.status(500).send(err);
+                }
             });
             return res.send('File updated!');
         } else {
-            return res.status(400).send('Password is invalid.');
+            return res.status(403).send('Password is invalid.');
         }
     }
 });
