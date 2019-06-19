c3156c6a00b73fb574352c7110b8865063862175
Евгений <evg.ishutin@gmail.com>
2019-06-19 16:52:50 +0300

    in download.js added work with versions, sending file by path in database bugs fixed

--- routes/download.js
+++ routes/download.js
@@ -7,9 +7,10 @@ router.get('/', async function (req, res) {
     const File = require('../lib/database').File;
     const name = req.query.name;
     const password = req.query.password;
+    const version = req.query.version;
 
     const filepath = path.normalize(__dirname + '/../files/' + name);
-    fs.stat(filepath, async function (err, stat) {
+    fs.stat(filepath,  async function (err, stat) {
         if (err || !stat.isFile()) {
             if (err.code === 'ENOENT') {
                 res.status(404);
@@ -18,12 +19,22 @@ router.get('/', async function (req, res) {
             }
             return res.send(err);
         }
-        const filedb = await File.findOne({name: name});
+        const filever = await File.count({name: name});
+        if(!version){
+            const filedb = await File.findOne({name: name, ver: filever});
+            if (password === filedb.password) {
+                return res.sendFile(filedb.path);
+            }
+        } else {
+            if (1 <= version && version <= filever) {
+                const filedb = await File.findOne({name: name, ver: version});
+                    if (password === filedb.password) {
+                        return res.sendFile(filedb.path);
+                    } else return res.status(403).send('Invalid password');
+                } else return res.status(403).send('Invalid version');
+            }
 
-        if (password === filedb.password) {
 
-            return res.sendFile(filepath);
-        }
     })
 });
 
