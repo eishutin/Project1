5bf716c03bdce8991c49b120f3c31f23cb1d7d42
Evhenii <evg.ishutin@gmail.com>
2019-06-28 21:07:08 +0300

    added hashing passwords using const salt

--- routes/download.js
+++ routes/download.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const passwordHash = require('password-hash');
+const salt = '$2b$31$pnU5ODd.F3Kk7TUy5tWhR.$2b$31$pnU5ODd.F3Kk7TUy5tWhR.';
 
 router.get('/', async function (req, res) {
     const File = require('../lib/database').File;
@@ -20,15 +22,14 @@ router.get('/', async function (req, res) {
         const filever = await File.count({name: name});
             if(!version){
                 const filedb = await File.findOne({name: name, version: filever});
-
-                if (password === filedb.password) {
+                if (passwordHash.verify(password + salt, filedb.password)) {
                         return res.sendFile(filedb.path);
                 } else return res.status(400).send('Invalid password')
 
         } else {
             if (version >=1 && version <= filever) {
                 const filedb = await File.findOne({name: name, version: version});
-                if (password === filedb.password) {
+                if (passwordHash.verify(password + salt, filedb.password)) {
                     return res.sendFile(filedb.path);
                 } else return res.status(400).send('Invalid password');
             } else return res.status(400).send('Invalid version');
--- routes/upload.js
+++ routes/upload.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const uuid = require('uuid/v4');
+const passwordHash = require('password-hash');
+const salt = '$2b$31$pnU5ODd.F3Kk7TUy5tWhR.$2b$31$pnU5ODd.F3Kk7TUy5tWhR.';
 
 router.post("/", async function (req, res) {
     const File = require('../lib/database').File;
 
     const name = req.query.id;
     const password = req.query.password;
-
     if (!name) {
         return res.status(400).send('Name is invalid.');
     }
@@ -29,26 +30,26 @@ router.post("/", async function (req, res) {
     }
 
     const filepath = path.normalize(__dirname + '/../files/' + uuid());
-
     if (!filedb) {
         sampleFile.mv(filepath, async function (err) {
             if (err)
-                return res.status(500).send(err);
+                return res.status(500).send(err + 'sadfg');
             try {
-                await File.create({name: name, password: password, path: filepath, version: 1});
+                const filehash = passwordHash.generate(password + salt);
+                console.log(filehash);
+                await File.create({name: name, password: filehash, path: filepath, version: 1});
                 return res.send('File uploaded!');
             } catch (e) {
-                return res.status(500).send(err);
+                return res.status(500).send(err + 'qwerty');
             }
         });
     } else {
-        if (password === filedb.password) {
+        if(passwordHash.verify(password + salt, filedb.password)){
             sampleFile.mv(filepath, async function (err) {
                 if (err)
                     return res.status(500).send(err);
                 try {
-                    await File.create({name: name, password: password, path: filepath, version: filever + 1});
-
+                    await File.create({name: name, password: filedb.password, path: filepath, version: filever + 1});
                 } catch (e) {
                     return res.status(500).send(err);
                 }
@@ -56,8 +57,7 @@ router.post("/", async function (req, res) {
             return res.send('File updated!');
         } else {
             return res.status(400).send('Password is invalid.');
-        }
-    }
+        }}
 });
 
 module.exports = router;
